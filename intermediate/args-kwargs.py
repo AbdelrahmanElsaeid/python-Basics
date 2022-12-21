50e1576d87a8d6b52899dea555e84ccce038fe88
abdelrahmanelsaeid <abdelrahmanelsaeid5@gmail.com>
2022-12-21 20:54:29 +0200

    *args

---
+++ intermediate/args-kwargs.py
@@ -0,0 +1,12 @@
+# The special syntax *args in function definitions in python is used to pass a variable number of arguments to a function.
+#  It is used to pass a non-key worded, variable-length argument list. 
+
+
+
+def sum(*args):
+    res = 0
+    for i in args:
+        res +=i
+    return res 
+
+print(sum(1,2,3,4,5))       
